4f851e0ad3ad7ac4768231242d2ddce6167a65ce
MisooKim <hanul1546@gmail.com>
2024-09-11 16:36:28 +0900

    candy_test

---
+++ challenge/week2/candy.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int m = 0;
+    int n = 0;
+    cin >> m;
+    cin >> n;
+    cout << "candy" << m/n;
+    return 0;
+}
