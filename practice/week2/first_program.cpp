eeb13a1a8150c71bc086225138e141314bbfbb26
Misoo Kim <hanul1546@gmail.com>
2024-09-12 17:27:24 +0900

    test

--- practice/week2/first_program.cpp
+++ practice/week2/first_program.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+	string name;
+	cout << "이름을 입력하시오: ";
+	cin >> name;
+	cout << name << "을 환영합니다." << endl;
+	return 0;
 }
-
- 
